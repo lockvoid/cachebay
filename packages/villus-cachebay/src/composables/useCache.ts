c43983b7dbcca08015c3b1ab0dfdd95083823b1a
Dmitrii Kochnev <dimitri@lockvoid.com>
2025-09-11 23:35:57 +0300

    12

--- packages/villus-cachebay/src/composables/useCache.ts
+++ packages/villus-cachebay/src/composables/useCache.ts
@@ -13,22 +13,22 @@ export { provideCachebay } from "../core/plugin";
 
 export function useCache(): {
   readFragment: CachebayInstance["readFragment"];
+  readFragments: CachebayInstance["readFragments"];
   writeFragment: CachebayInstance["writeFragment"];
   identify: CachebayInstance["identify"];
   modifyOptimistic: CachebayInstance["modifyOptimistic"];
   inspect: {
     entities: (typename?: string) => string[];
-    get: (key: string) => any;
+    entity: (key: string) => any;
     connections: () => string[];
     connection: (
       parent: "Query" | { __typename: string; id?: any; _id?: any },
       field: string,
       variables?: Record<string, any>,
     ) => any;
+    operations: () => string[];
   };
   hasFragment: (refOrKey: string | { __typename: string; id?: any; _id?: any }) => boolean;
-  listEntityKeys: (selector: string | string[]) => string[];
-  listEntities: (selector: string | string[], materialized?: boolean) => any[];
   entitiesTick: Ref<number>;
 } {
   const api = inject<any>(CACHEBAY_KEY, null);
