976ed17a53968f2aded84a2713a2a354b23a8ee6
Dmitrii Kochnev <dimitri@lockvoid.com>
2025-10-01 04:34:55 +0300

    123

--- packages/demo/bin/reset-db.sh
+++ packages/demo/bin/reset-db.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env sh
+set -euo pipefail
 
 mkdir -p /app/data
 
-cp /app/vendor-template/harrypotter-origin.db /app/data/harrypotter.db
+cp /app/vendor/harrypotter-origin.db /app/data/harrypotter.db
 
 chmod 666 /app/data/harrypotter.db
 
-echo "Database ready at $(date)"
+echo "Database reset at $(date)"
