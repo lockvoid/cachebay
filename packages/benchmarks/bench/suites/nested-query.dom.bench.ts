93814133cddbc9c5c65ea7895460a24e88386f66
Dmitrii Kochnev <dimitri@lockvoid.com>
2025-10-26 22:06:31 +0200

    Update nested-query.dom.bench.ts

--- packages/benchmarks/bench/suites/nested-query.dom.bench.ts
+++ packages/benchmarks/bench/suites/nested-query.dom.bench.ts
@@ -1,10 +1,10 @@
 import { bench, describe } from "vitest";
-import { createReactRelayNestedApp } from "../src/ui/react-relay-nested-query-app";
-import { createVueApolloNestedApp } from "../src/ui/vue-apollo-nested-query-app";
-import { createVueCachebayNestedApp } from "../src/ui/vue-cachebay-nested-query-app";
-import { createVueUrqlNestedApp } from "../src/ui/vue-urql-nested-query-app";
-import { createNestedYoga } from "../src/server/nested-query-server";
-import { makeNestedDataset } from "../src/utils/seed-nested-query";
+import { createReactRelayNestedApp } from "../../src/ui/react-relay-nested-query-app";
+import { createVueApolloNestedApp } from "../../src/ui/vue-apollo-nested-query-app";
+import { createVueCachebayNestedApp } from "../../src/ui/vue-cachebay-nested-query-app";
+import { createVueUrqlNestedApp } from "../../src/ui/vue-urql-nested-query-app";
+import { createNestedYoga } from "../../src/server/nested-query-server";
+import { makeNestedDataset } from "../../src/utils/seed-nested-query";
 import Table from 'cli-table3';
 
 const DEBUG = true;
