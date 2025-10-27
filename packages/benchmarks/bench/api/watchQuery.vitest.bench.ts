cb681aee650511b3d5a2b5424a93a6ac8a8c8514
Dmitrii Kochnev <dimitri@lockvoid.com>
2025-10-27 12:04:23 +0200

    Update watchQuery.vitest.bench.ts

--- packages/benchmarks/bench/api/watchQuery.vitest.bench.ts
+++ packages/benchmarks/bench/api/watchQuery.vitest.bench.ts
@@ -57,7 +57,7 @@ const createApollo = () => {
 const createRelay = () => {
   return new Environment({ network: Network.create(async () => ({})), store: new Store(new RecordSource()) });
 };
-/*
+
 describe('watchQuery (initial:cold)', () => {
   const pages = buildPages({ data: buildUsersResponse({ users: 1000, posts: 5, comments: 3 }), pageSize: 10 });
   const iterations = [];
@@ -81,7 +81,7 @@ describe('watchQuery (initial:cold)', () => {
     setup() {
       iterations.length = 0;
 
-      for (let i = 0; i < (ITERATIONS + 10) * 5; i++) {
+      for (let i = 0; i < (ITERATIONS + 10) * 20; i++) {
         const cachebay = createCachebay();
 
         iterations.push({ cachebay });
@@ -107,107 +107,10 @@ describe('watchQuery (initial:cold)', () => {
     setup() {
       iterations.length = 0;
 
-      for (let i = 0; i < (ITERATIONS + 10) * 5; i++) {
+      for (let i = 0; i < (ITERATIONS + 10) * 20; i++) {
         const relay = createRelay();
 
         const operation = createOperationDescriptor(USERS_RELAY_QUERY, { first: 10, after: null });
-
-        iterations.push({ relay, operation });
-      }
-    }
-  });
-});*/
-
-describe('watchQuery (initial:hot)', () => {
-  const pages = buildPages({ data: buildUsersResponse({ users: 100, posts: 5, comments: 3 }), pageSize: 10 });
-  const iterations = [];
-
-  bench('cachebay - watchQuery', () => {
-    const { cachebay } = iterations.pop();
-
-    const subscription = cachebay.watchQuery({
-      query: USERS_CACHEBAY_QUERY,
-      variables: { first: 10, after: null },
-    });
-
-    subscription.unsubscribe();
-  }, {
-    iterations: ITERATIONS,
-    warmupIterations: 2,
-
-    setup() {
-      iterations.length = 0;
-
-      for (let i = 0; i < (ITERATIONS + 10) * 5; i++) {
-        const cachebay = createCachebay();
-
-        for (let j = 0; j < pages.length; j++) {
-          cachebay.writeQuery({ query: USERS_CACHEBAY_QUERY, variables: pages[j].variables, data: pages[j].data });
-        }
-
-        cachebay.readQuery({ query: USERS_CACHEBAY_QUERY, variables: { first: 10, after: null }, canonical: true });
-
-        iterations.push({ cachebay });
-      }
-    }
-  });
-
-  bench('apollo - watch', () => {
-    const { apollo } = iterations.pop();
-
-    const observable = apollo.watch({
-      query: USERS_APOLLO_QUERY,
-      variables: { first: 10, after: null },
-      optimistic: false,
-    });
-
-    const subscription = observable.subscribe(() => {});
-
-    subscription.unsubscribe()
-  }, {
-    iterations: ITERATIONS,
-    warmupIterations: 2,
-
-    setup() {
-      iterations.length = 0;
-
-      console.log('tttt')
-      for (let i = 0; i < (ITERATIONS + 10) * 5; i++) {
-        const apollo = createApollo();
-
-        for (let i = 0; i < pages.length; i++) {
-          apollo.writeQuery({ query: USERS_APOLLO_QUERY, variables: pages[i].variables, data: pages[i].data });
-        }
-
-        iterations.push({ apollo });
-      }
-    }
-  });
-
-  bench('relay - subscribe', () => {
-    const { relay, operation } = iterations.pop();
-
-    const snapshot = relay.lookup(operation.fragment);
-    const disposable = relay.subscribe(snapshot, () => {});
-
-    disposable.dispose();
-  }, {
-    iterations: ITERATIONS,
-    warmupIterations: 2,
-
-    setup() {
-      iterations.length = 0;
-
-      for (let i = 0; i < (ITERATIONS + 10) * 5; i++) {
-        const relay = createRelay();
-
-        for (let j = 0; j < pages.length; j++) {
-          relay.commitPayload(createOperationDescriptor(USERS_RELAY_QUERY, pages[j].variables), pages[j].data);
-        }
-
-        const operation = createOperationDescriptor(USERS_RELAY_QUERY, { first: 10, after: null });
-
-        relay.lookup(operation.fragment);
 
         iterations.push({ relay, operation });
       }
