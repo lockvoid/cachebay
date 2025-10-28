74050fe26a7a6ac8b70801eacd1e2598dc693b59
Dmitrii Kochnev <dimitri@lockvoid.com>
2025-10-28 12:41:59 +0200

    Update useSpellsQuery.ts

--- packages/demo/app/composables/useSpellsQuery.ts
+++ packages/demo/app/composables/useSpellsQuery.ts
@@ -64,7 +64,9 @@ export const useSpellsQuery = async () => {
   const query = await useQuery({ query: settings.relayMode === "infinite" ? SPELLS_QUERY_INFINITE_MODE : SPELLS_QUERY_PAGE_MODE, variables });
 
   watch(pagination.filter, () => {
-    activity.isFetching = true;
+    setTimeout(() => {
+      activity.isFetching = query.isFetching.value;
+    }, 0);
   });
 
   watch(query.isFetching, (isFetching) => {
