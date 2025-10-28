c3ead38f0fdf3d32d7dd01890cdec30d34a400e8
Dmitrii Kochnev <dimitri@lockvoid.com>
2025-10-28 07:41:09 +0200

    12

--- packages/cachebay/src/core/constants.ts
+++ packages/cachebay/src/core/constants.ts
@@ -18,20 +18,24 @@ export const CACHE_ONLY = "cache-only" as const;
 
 export const CONNECTION_FIELDS = new Set(["first", "last", "after", "before"]);
 
-export const CONNECTION_EDGES = "edges" as const;
+export const CONNECTION_EDGES_FIELD = "edges" as const;
 
-export const CONNECTION_PAGE_INFO = "pageInfo" as const;
+export const CONNECTION_PAGE_INFO_FIELD = "pageInfo" as const;
 
-export const CONNECTION_FIRST = "first" as const;
+export const CONNECTION_FIRST_FIELD = "first" as const;
 
-export const CONNECTION_LAST = "last" as const;
+export const CONNECTION_LAST_FIELD = "last" as const;
 
-export const CONNECTION_AFTER = "after" as const;
+export const CONNECTION_AFTER_FIELD = "after" as const;
 
-export const CONNECTION_BEFORE = "before" as const;
+export const CONNECTION_BEFORE_FIELD = "before" as const;
 
 export const CONNECTION_DIRECTIVE = "connection";
 
 export const CONNECTION_MODE_INFINITE = "infinite";
 
 export const CONNECTION_MODE_PAGE = "page";
+
+export const CONNECTION_TYPENAME = "Connection" as const;
+
+export const CONNECTION_PAGE_INFO_TYPENAME = "PageInfo" as const;
